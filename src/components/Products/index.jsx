55520ec4c6b91d5f759c36d03576978b7b71b0eb
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-05 20:38:56 +0400

    added link for product detail page

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 const Products = () => {
   const url = "http://localhost:3000/products";
@@ -12,30 +13,36 @@ const Products = () => {
   }, []);
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-6 px-5">
-      {products.map(({ id, title, images, price, category, description }) => {
-        return (
-          <div
-            key={id}
-            className="border-1 border-gray-300 relative rounded-md"
-          >
+      {products.map(
+        ({ id, title, images, price, category, description, slug }) => {
+          return (
             <div
-              alt={title}
-              className="object-con w-full h-[200px] rounded-tl-md rounded-tr-md bg-center bg-no-repeat bg-cover"
-              style={{ backgroundImage: `url(${images[0]})` }}
-            />
-            <div className="p-2">
-              <h2 className="truncate font-bold">{title}</h2>
-              <h2 className="absolute top-2 right-2 bg-red-300 px-2 py-0.5 text-sm rounded-md">
-                {category.name}
-              </h2>
-              <h2 className="my-2 line-clamp-2">{description}</h2>
-              <h2 className="truncate text-red-700 text-2xl font-bold">
-                {price} AZN
-              </h2>
+              key={id}
+              className="border-1 border-gray-300 relative rounded-md"
+            >
+              <div
+                alt={title}
+                className="object-con w-full h-[200px] rounded-tl-md rounded-tr-md bg-center bg-no-repeat bg-cover"
+                style={{ backgroundImage: `url(${images[0]})` }}
+              />
+              <div className="p-2">
+                <h2 className="truncate font-bold">{title}</h2>
+                <h2 className="absolute top-2 right-2 bg-red-300 px-2 py-0.5 text-sm rounded-md">
+                  {category.name}
+                </h2>
+                <h2 className="my-2 line-clamp-2">{description}</h2>
+                <h2 className="truncate text-red-700 text-2xl font-bold">
+                  {price} AZN
+                </h2>
+              </div>
+              <Link
+                to={"/products/" + slug}
+                className="absolute inset-0"
+              ></Link>
             </div>
-          </div>
-        );
-      })}
+          );
+        }
+      )}
     </div>
   );
 };
