aa664f6897c999edea527f9174ef02b453cb04bb
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-05 21:27:16 +0400

    rating componetn created

---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const RatingStar = ({ filled }) => {
+  return (
+    <span
+      className={`star ${filled ? "filled" : "empty"}`}
+      style={{
+        fontSize: "30px",
+        marginRight: "5px",
+        color: filled ? "gold" : "black",
+      }}
+    >
+      &#9733;
+    </span>
+  );
+};
+
+const Rating = ({ totalStars = 5, rating = 3, ratingId = "" }) => {
+  return (
+    <div>
+      {Array.from({ length: totalStars }, (_, index) => (
+        <RatingStar key={index} filled={index < rating} />
+      ))}
+
+      <div style={{ marginTop: "10px", fontSize: "16px", color: "gray" }}>
+        Reytinq: {rating} / {totalStars}
+      </div>
+    </div>
+  );
+};
+
+export default Rating;
