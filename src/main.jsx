e8186f6209107f0b7d24717369d76ecf99b14f6e
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-07 19:59:41 +0400

    imported hot toast

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter } from "react-router";
 import "./App.css";
 import App from "./App.jsx";
 import { createRoot } from "react-dom/client";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <Toaster position="top-right" />
     <App />
   </BrowserRouter>
 );
