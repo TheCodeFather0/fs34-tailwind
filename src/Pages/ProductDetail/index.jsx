0be60958c6bee3a1ea039bcf629964f5a51df6b5
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-07 20:00:02 +0400

    added hot toast

--- src/Pages/ProductDetail/index.jsx
+++ src/Pages/ProductDetail/index.jsx
@@ -3,6 +3,8 @@ import Navbar from "../../components/Navbar";
 import { useParams } from "react-router";
 import React, { useEffect, useState } from "react";
 import Rating from "../../components/Rating";
+import toast from "react-hot-toast";
+import DataNotFound from "../../components/DataNotFound";
 
 const ProductDetail = () => {
   const { slug } = useParams();
@@ -11,44 +13,57 @@ const ProductDetail = () => {
   const [currentImage, setCurrentImage] = useState("");
 
   useEffect(() => {
-    axios.get(url).then(({ data }) => {
-      const currentElement = data.find((e) => e.slug === slug);
-      setProduct(currentElement);
-      setCurrentImage(currentElement.images[0]);
-    });
+    axios
+      .get(url)
+      .then(({ data }) => {
+        const currentElement = data.find((e) => e.slug === slug);
+        setProduct(currentElement);
+        setCurrentImage(currentElement.images[0]);
+      })
+      .catch((err) => {
+        if (err.status === 404) {
+          toast.error("backend ilə bağlı xəta!");
+        }
+      });
   }, []);
 
   return (
     <>
       <Navbar />
 
-      <div className="grid grid-cols-2 px-5 gap-16 mt-4">
-        <div className="border-2 border-gray-300 p-8 grid grid-cols-[1fr_100px] gap-5">
-          <img src={currentImage} />
-          <div className="grid grid-cols-[100px] grid-rows-[100px_100px_100px] object-cover gap-3">
-            {product.images?.map((image) => {
-              return (
-                <img
-                  src={image}
-                  className="w-full h-full cursor-pointer"
-                  onClick={() => {
-                    setCurrentImage(image);
-                  }}
-                />
-              );
-            })}
+      {product.slug ? (
+        <div className="grid grid-cols-2 px-5 gap-16 mt-4">
+          <div className="border-2 border-gray-300 p-8 grid grid-cols-[1fr_100px] gap-5">
+            <img src={currentImage} />
+            <div className="grid grid-cols-[100px] grid-rows-[100px_100px_100px] object-cover gap-3">
+              {product.images?.map((image) => {
+                return (
+                  <img
+                    src={image}
+                    className="w-full h-full cursor-pointer"
+                    onClick={() => {
+                      setCurrentImage(image);
+                    }}
+                  />
+                );
+              })}
+            </div>
+          </div>
+          <div>
+            <h2 className="text-4xl font-bold text-slate-500">
+              {product.title}
+            </h2>
+            <h2 className="my-3 text-xl">{product.description}</h2>
+
+            <h2 className="text-4xl text-red-500 font-bold">
+              {product.price} AZN
+            </h2>
+            <Rating rating={product.rating} />
           </div>
         </div>
-        <div>
-          <h2 className="text-4xl font-bold text-slate-500">{product.title}</h2>
-          <h2 className="my-3 text-xl">{product.description}</h2>
-
-          <h2 className="text-4xl text-red-500 font-bold">
-            {product.price} AZN
-          </h2>
-          <Rating rating={product.rating} />
-        </div>
-      </div>
+      ) : (
+        <DataNotFound />
+      )}
     </>
   );
 };
--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,49 +1,64 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 import { Link } from "react-router";
+import DataNotFound from "../DataNotFound";
 
 const Products = () => {
   const url = import.meta.env.VITE_BACKEND_URL;
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    axios.get(url).then(({ data }) => {
-      setProducts(data);
-    });
+    axios
+      .get(url)
+      .then(({ data }) => {
+        setProducts(data);
+      })
+      .catch((err) => {
+        if (err.status === 404) {
+          toast.error("backend ilə bağlı xəta!");
+        }
+      });
   }, []);
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-6 px-5">
-      {products.map(
-        ({ id, title, images, price, category, description, slug }) => {
-          return (
-            <div
-              key={id}
-              className="border-1 border-gray-300 relative rounded-md"
-            >
-              <div
-                alt={title}
-                className="object-con w-full h-[200px] rounded-tl-md rounded-tr-md bg-center bg-no-repeat bg-cover"
-                style={{ backgroundImage: `url(${images[0]})` }}
-              />
-              <div className="p-2">
-                <h2 className="truncate font-bold">{title}</h2>
-                <h2 className="absolute top-2 right-2 bg-red-300 px-2 py-0.5 text-sm rounded-md">
-                  {category.name}
-                </h2>
-                <h2 className="my-2 line-clamp-2">{description}</h2>
-                <h2 className="truncate text-red-700 text-2xl font-bold">
-                  {price} AZN
-                </h2>
-              </div>
-              <Link
-                to={"/products/" + slug}
-                className="absolute inset-0"
-              ></Link>
-            </div>
-          );
-        }
+    <>
+      {products.length > 0 ? (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 my-6 px-5">
+          {products.map(
+            ({ id, title, images, price, category, description, slug }) => {
+              return (
+                <div
+                  key={id}
+                  className="border-1 border-gray-300 relative rounded-md"
+                >
+                  <div
+                    alt={title}
+                    className="object-con w-full h-[200px] rounded-tl-md rounded-tr-md bg-center bg-no-repeat bg-cover"
+                    style={{ backgroundImage: `url(${images[0]})` }}
+                  />
+                  <div className="p-2">
+                    <h2 className="truncate font-bold">{title}</h2>
+                    <h2 className="absolute top-2 right-2 bg-red-300 px-2 py-0.5 text-sm rounded-md">
+                      {category.name}
+                    </h2>
+                    <h2 className="my-2 line-clamp-2">{description}</h2>
+                    <h2 className="truncate text-red-700 text-2xl font-bold">
+                      {price} AZN
+                    </h2>
+                  </div>
+                  <Link
+                    to={"/products/" + slug}
+                    className="absolute inset-0"
+                  ></Link>
+                </div>
+              );
+            }
+          )}
+        </div>
+      ) : (
+        <DataNotFound />
       )}
-    </div>
+    </>
   );
 };
 
