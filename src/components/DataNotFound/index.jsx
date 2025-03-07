7846ab337fa7b25c1ffc4586360fd85096c5260b
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-07 19:59:51 +0400

    data not found component created

---
+++ src/components/DataNotFound/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DataNotFound = () => {
+  return (
+    <div className="p-8 text-red-500 text-5xl">
+      İstədiyiniz məlumat tapılmadı!
+    </div>
+  );
+};
+
+export default DataNotFound;
