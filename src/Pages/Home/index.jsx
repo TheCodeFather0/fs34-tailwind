80533534c6c99285736637af788fa791c8ea4363
TheCodeFather0 <raminmammadov056@gmail.com>
2025-03-10 20:29:56 +0400

    search and category filter added

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar";
 import Products from "../../components/Products";
+import Categories from "../../components/Categories";
 
 const Home = () => {
+  const [searchedText, setSearchedText] = useState("");
+  const [activeCategory, setActiveCategory] = useState("All");
+
   return (
     <>
-      <Navbar />
-      <Products />
+      <Navbar searchedText={searchedText} setSearchedText={setSearchedText} />
+      <div className="px-5 grid grid-cols-[200px_1fr]">
+        <Categories
+          activeCategory={activeCategory}
+          setActiveCategory={setActiveCategory}
+        />
+        <Products
+          searchedText={searchedText}
+          activeCategory={activeCategory}
+          setActiveCategory={setActiveCategory}
+        />
+      </div>
     </>
   );
 };
